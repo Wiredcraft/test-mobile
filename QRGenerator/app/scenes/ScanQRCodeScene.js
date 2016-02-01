d2a2d8776ee28fcdf3c4b477064183526908d2c1
Ryan Chu <ryaneof@gmail.com>
2016-02-01 09:28:41 +0800

    Add scanning boundary for scan QR code scene.

--- QRGenerator/app/scenes/ScanQRCodeScene.js
+++ QRGenerator/app/scenes/ScanQRCodeScene.js
@@ -1,7 +1,7 @@
 import React, {
   StyleSheet,
   AlertIOS,
-  Text,
+  View,
   Component,
   PropTypes
 } from 'react-native';
@@ -19,6 +19,60 @@ const styles = StyleSheet.create({
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
+  },
+  scanningBorderContainer: {
+    width: 200,
+    height: 200,
+    flexDirection: 'column',
+    justifyContent: 'space-between'
+  },
+  scanningFirstRow: {
+    width: 196,
+    flexDirection: 'row',
+    alignSelf: 'flex-start',
+    justifyContent: 'space-between'
+  },
+  scanningLastRow: {
+    width: 196,
+    flexDirection: 'row',
+    alignSelf: 'flex-end',
+    justifyContent: 'space-between'
+  },
+  scanningBorderLeftTop: {
+    alignSelf: 'flex-start',
+    width: 32,
+    height: 32,
+    borderTopWidth: 2,
+    borderLeftWidth: 2,
+    borderColor: '#eee',
+    opacity: 0.9
+  },
+  scanningBorderRightTop: {
+    alignSelf: 'flex-end',
+    width: 32,
+    height: 32,
+    borderTopWidth: 2,
+    borderRightWidth: 2,
+    borderColor: '#eee',
+    opacity: 0.9
+  },
+  scanningBorderLeftBottom: {
+    alignSelf: 'flex-start',
+    width: 32,
+    height: 32,
+    borderBottomWidth: 2,
+    borderLeftWidth: 2,
+    borderColor: '#eee',
+    opacity: 0.9
+  },
+  scanningBorderRightBottom: {
+    alignSelf: 'flex-end',
+    width: 32,
+    height: 32,
+    borderBottomWidth: 2,
+    borderRightWidth: 2,
+    borderColor: '#eee',
+    opacity: 0.9
   }
 });
 
@@ -65,9 +119,16 @@ class ScanQRCodeScene extends Component {
         style={ styles.container }
         onBarCodeRead={ this.onBarCodeRead }
       >
-        <Text style={ styles.scanTip }>
-          Scanning!
-        </Text>
+        <View style={ styles.scanningBorderContainer }>
+          <View style={ styles.scanningFirstRow }>
+            <View style={ styles.scanningBorderLeftTop } />
+            <View style={ styles.scanningBorderRightTop } />
+          </View>
+          <View style={ styles.scanningLastRow }>
+            <View style={ styles.scanningBorderLeftBottom } />
+            <View style={ styles.scanningBorderRightBottom } />
+          </View>
+        </View>
       </Camera>
     );
   }
