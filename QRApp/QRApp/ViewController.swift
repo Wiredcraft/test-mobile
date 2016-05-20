16bdaf8537e5e0e061fe57a4f7633e09a0a8713e
顾强 <guqiang@ycode.cn>
2016-05-20 16:52:35 +0800

    add animation

--- QRApp/QRApp/ViewController.swift
+++ QRApp/QRApp/ViewController.swift
@@ -8,34 +8,76 @@
 
 import UIKit
 
-class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
-    @IBOutlet weak var tableView: UITableView!
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var cloudBtn: UIButton!
+    @IBOutlet weak var cameraBtn: UIButton!
     @IBOutlet weak var menuBtn: UIButton!
+    
+    @IBOutlet weak var cloudBtnPosition: NSLayoutConstraint!
+    @IBOutlet weak var cameraBtnPosition: NSLayoutConstraint!
+    
+    private struct MenuAnimationKeys{
+        static var CameraBtnOutTimeInterval:NSTimeInterval = 0.5
+        static var CloudBtnOutTimeInterval:NSTimeInterval = 0.3
+        static var CameraBtnOutPosition:CGFloat = 10
+        static var CloudBtnOutPosition:CGFloat = 70
+        static var CameraBtnInPosition:CGFloat = -50
+        static var CloudBtnInPosition:CGFloat = 10
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         self.edgesForExtendedLayout = UIRectEdge.None
-        self.tableView.delegate = self
-        self.tableView.dataSource = self
-        self.tableView.rowHeight = 44
-        self.tableView.registerClass(UITableViewCell.classForKeyedArchiver(), forCellReuseIdentifier: "cell")
+        self.navigationController?.navigationBar.translucent = false
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+    @IBAction func menuBtn_Pressed(sender: UIButton) {
+        if sender.selected {
+            self.cameraBtn.hidden = false
+            UIView.animateWithDuration(MenuAnimationKeys.CameraBtnOutTimeInterval, animations: {
+                self.cameraBtnPosition.constant = MenuAnimationKeys.CameraBtnOutPosition
+                self.view.layoutIfNeeded()
+            }) { (success) in
+                self.cloudBtn.hidden = false
+                UIView.animateWithDuration(MenuAnimationKeys.CloudBtnOutTimeInterval, animations: {
+                    self.cloudBtnPosition.constant = MenuAnimationKeys.CloudBtnOutPosition
+                    self.view.layoutIfNeeded()
+                    }, completion: { (success) in
+                        self.menuBtn.setImage(UIImage.init(named: "menuOut"), forState: UIControlState.Normal)
+                })
+            }
+        }else{
+            UIView.animateWithDuration(MenuAnimationKeys.CloudBtnOutTimeInterval, animations: {
+                self.cloudBtnPosition.constant = MenuAnimationKeys.CloudBtnInPosition
+                self.view.layoutIfNeeded()
+            }) { (success) in
+                self.cloudBtn.hidden = true
+                UIView.animateWithDuration(MenuAnimationKeys.CameraBtnOutTimeInterval, animations: {
+                    self.cameraBtnPosition.constant = MenuAnimationKeys.CameraBtnInPosition
+                    self.view.layoutIfNeeded()
+                    }, completion: { (success) in
+                        self.cameraBtn.hidden = true
+                        self.menuBtn.setImage(UIImage.init(named: "menuIn"), forState: UIControlState.Normal)
+                })
+            }
+        }
+        sender.selected = !sender.selected
+    }
+
+    func menuSwitch(open:Bool){
+        print("sss")
     }
     
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("cell")
-        cell?.textLabel?.text = "\(indexPath.row)"
-        return cell!
-    }
+
 }
 
