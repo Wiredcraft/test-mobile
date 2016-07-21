2d8152696c89853e24c254ffab8b3a8eb477de09
guoqing.zgg <guoqing.zgg@alibaba-inc.com>
2016-07-21 09:00:19 +0800

    add a fake seed api to server.

--- server/index.js
+++ server/index.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
 
+app.get('/seed', (req, res) => {
+    // todo: return a fake seed for now.
+    const reply = {
+        seed: Date.now(),
+        expiredTimeout: 60
+    };
+    res.send(JSON.stringify(reply));
+});
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
