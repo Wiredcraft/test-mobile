bc8e0d13cfe3a3804005e34ff4762146cef9adb6
shiyinayuriko <whitecometliliwill@gmail.com>
2016-07-22 00:03:07 +0800

    seed generator implement

--- wRandomSeedGenerator/routes/seed.js
+++ wRandomSeedGenerator/routes/seed.js
@@ -2,8 +2,33 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.json({msg:'seed'});
+router.get('/test/:seed/:exptime', function(req, res, next) {
+	res.json({seed:req.params.seed,expiredTime:req.params.exptime});
 });
+router.get('/test/:seed', function(req, res, next) {
+	if (!req.exp) {
+		req.params.exptime = 123;
+	};
+	res.json({seed:req.params.seed,expiredTime:getExptime()});
+});
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+	res.json({seed:getRandomSeed(16,32),expiredTime:getExptime()});
+});
+
+
+var EXPIRED_TIME = 1000 * 60 * 60;
+function getExptime(){
+	return new Date().getTime() + EXPIRED_TIME;
+}
+function getRandomSeed(radix, len){
+	var ret = '';
+	while(ret.length < len){
+		ret += Math.random().toString(radix).substr(2);
+	}
+	console.log( ret );
+	return ret.substr(ret.length - len);
+}
 
 module.exports = router;
