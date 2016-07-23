59c361b8fb819d1d31a2f4f6de806e4b98e3bb78
guoqing.zgg <guoqing.zgg@alibaba-inc.com>
2016-07-23 21:50:10 +0800

    use fake server data from the doc.

--- server/index.js
+++ server/index.js
@@ -4,8 +4,8 @@ const app = express();
 app.get('/seed', (req, res) => {
     // todo: return a fake seed for now.
     const reply = {
-        seed: Date.now(),
-        expiredTimeout: 60
+        seed: "37790a1b728096b4141864f49159ad47",
+        expiredAt: Date.now() + 60000
     };
     res.send(JSON.stringify(reply));
 });
