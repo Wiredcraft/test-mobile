1ed8f5080a3d7603a218a5d767a433fac4d66ac2
guoqing.zgg <guoqing.zgg@alibaba-inc.com>
2016-07-24 21:17:33 +0800

    add /validate post server api.

--- server/index.js
+++ server/index.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const app = express();
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
 const SeedManager = require('./SeedManager');
 const seedManager = new SeedManager(6000, 1000);
 
 app.get('/seed', (req, res) => {
     const reply = seedManager.getSeed();
+    res.send(JSON.stringify(reply));
+});
+app.post('/validate', (req, res) => {
+    const seed = req.body.seed;
+    const result = seedManager.validateSeed(seed);
+    const reply = {result};
     res.send(JSON.stringify(reply));
 });
 
