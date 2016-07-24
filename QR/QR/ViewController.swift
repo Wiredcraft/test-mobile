8f62a08264ef7ace56be466ddc45d1e9d8674ad5
guoqing.zgg <guoqing.zgg@alibaba-inc.com>
2016-07-25 07:49:25 +0800

    implement validate seed ui logic. add PKHUD.

--- QR/QR/ViewController.swift
+++ QR/QR/ViewController.swift
@@ -9,14 +9,33 @@
 import UIKit
 import AVFoundation
 import QRCodeReader
+import PKHUD
 
 class ViewController: UIViewController {
     lazy var readerVC = QRCodeReaderViewController(metadataObjectTypes: [AVMetadataObjectTypeQRCode])
 
+    func validateSeed(seed:String) {
+        HUD.show(.LabeledProgress(title:"validating seed", subtitle:seed))
+        
+        let seedManager = SeedManager()
+        seedManager.validateSeed(seed, callback: { (valid) -> Void in
+            var type: HUDContentType
+            if valid == nil {
+                type = .LabeledError(title:"validate failed", subtitle: nil)
+            } else if valid! {
+                type = .LabeledSuccess(title:"valid seed", subtitle: seed)
+            } else {
+                type = .LabeledError(title:"invalid seed", subtitle: seed)
+            }
+            HUD.flash(type, delay: 2.0) { _ in
+                self.navigationController?.popViewControllerAnimated(true)
+            }
+        })
+    }
     @IBAction func scanQR(sender: AnyObject) {
         readerVC.completionBlock = { (result: String?) in
-            print(result)
-            self.navigationController?.popViewControllerAnimated(true)
+            guard let seed = result else { return }
+            self.validateSeed(seed)
         }
         
         self.navigationController?.pushViewController(readerVC, animated: true)
@@ -31,7 +50,6 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
 
