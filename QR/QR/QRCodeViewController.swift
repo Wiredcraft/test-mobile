058c2e9bdca2b8b185b1fc4b3747c057f3169f3a
guoqing.zgg <guoqing.zgg@alibaba-inc.com>
2016-07-24 12:34:22 +0800

    add QRCodeViewController and its UI.

---
+++ QR/QR/QRCodeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  QRCodeViewController.swift
+//  QR
+//
+//  Created by Samuel Zhang on 7/24/16.
+//  Copyright © 2016 Samuel Zhang. All rights reserved.
+//
+
+import UIKit
+
+class QRCodeViewController: UIViewController {
+
+    @IBOutlet weak var timeoutLabel: UILabel!
+    @IBOutlet weak var qrImageView: UIImageView!
+    
+}
