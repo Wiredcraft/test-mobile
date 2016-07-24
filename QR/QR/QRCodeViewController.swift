6318d3f745c1f6d7ea21416449529b457558d0de
guoqing.zgg <guoqing.zgg@alibaba-inc.com>
2016-07-24 12:53:34 +0800

    implement load QR image logic.

--- QR/QR/QRCodeViewController.swift
+++ QR/QR/QRCodeViewController.swift
@@ -7,10 +7,51 @@
 //
 
 import UIKit
+import CoreImage
+
+func createQRFromString(str: NSString) -> UIImage? {
+    let stringData = str.dataUsingEncoding(NSUTF8StringEncoding)
+    let filter = CIFilter(name: "CIQRCodeGenerator")
+    filter?.setValue(stringData, forKey: "inputMessage")
+    filter?.setValue("H", forKey: "inputCorrectionLevel")
+
+    let image = filter?.outputImage?.imageByApplyingTransform(CGAffineTransformMakeScale(5.0, 5.0))
+    if let image = image {
+        return UIImage(CIImage:image, scale:1.0, orientation:UIImageOrientation.Down)
+    } else {
+        return nil
+    }
+}
 
 class QRCodeViewController: UIViewController {
 
     @IBOutlet weak var timeoutLabel: UILabel!
     @IBOutlet weak var qrImageView: UIImageView!
     
+    let seedManager = SeedManager()
+    var theModel: SeedModel?
+    
+    func loadUI() {
+        print("getSeedAsync")
+        seedManager.getSeedAsync { (model) -> Void in
+            print("getSeedAsync callback")
+            guard let model = model else {
+                self.timeoutLabel.text = "failed to load data"
+                return
+            }
+            self.theModel = model
+            if let img = createQRFromString(model.seed) {
+                self.qrImageView.image = img
+            }
+            self.startSeedTimeout()
+        }
+    }
+    
+    func startSeedTimeout() {}
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.loadUI()
+    }
+
 }
