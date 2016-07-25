45559cb3bd27cc1b8df66196394b79125fa54278
guoqing.zgg <guoqing.zgg@alibaba-inc.com>
2016-07-25 21:30:43 +0800

    refactor SeedManager for better design.

--- QR/QR/SeedManager.swift
+++ QR/QR/SeedManager.swift
@@ -11,7 +11,8 @@ import Alamofire
 
 public class SeedManager {
     let seedKey = "seedKey"
-    static let apiUrl = "http://10.126.52.103:3000"
+    static let apiUrl = "http://localhost:3000"
+//    static let apiUrl = "http://10.126.52.103:3000"
     let getSeedUrl = "\(apiUrl)/seed"
     let validateSeedUrl = "\(apiUrl)/validate"
 
@@ -20,47 +21,45 @@ public class SeedManager {
 
     init() {}
 
-    func getDataFromServer(callback: (NSData?) -> Void) {
+    func getModelFromServer(callback: (SeedModel?) -> Void) {
         let task = session.dataTaskWithURL(NSURL(string: getSeedUrl)!, completionHandler: {
             (data, response, error) -> Void in
-            callback(data)
+            callback(SeedModel(data:data))
         })
         task.resume()
     }
-    
-    func getDataFromCache() -> NSData? {
-        let data = userDefaults.objectForKey(seedKey)
-        return data as? NSData
+
+    func getModelFromCache() -> SeedModel? {
+        let data = userDefaults.objectForKey(seedKey) as? NSData
+        return SeedModel(data:data)
     }
-    
-    func cacheData(data: NSData) {
-        userDefaults.setObject(data, forKey: seedKey)
+
+    func cacheModel(model: SeedModel) {
+        userDefaults.setObject(model.rawData, forKey: seedKey)
     }
-    
+
     public func getSeedAsync(callback: (SeedModel?) -> Void) {
         func runCallbackOnMainThread(model: SeedModel?) {
             dispatch_async(dispatch_get_main_queue()) { () -> Void in
                 callback(model)
             }
         }
-        
-        let data = getDataFromCache()
-        // if there's a cached model, and it will not expire soon (in 1s), return it. 
-        if let model = SeedModel(data: data) where model.expireTimeout() > 1.0 {
+
+        // if there's a cached model, and it will not expire soon (in 1s), return it.
+        if let model = getModelFromCache() where model.expireTimeout() > 1.0 {
             print("getSeedAsync returns cached model \(model)")
             runCallbackOnMainThread(model)
             return
         }
-        getDataFromServer { (data) -> Void in
-            let model = SeedModel(data: data)
+        getModelFromServer { (model) -> Void in
             print("getSeedAsync returns model from server \(model)")
             if let model = model where model.expireTimeout() > 0 {
-                self.cacheData(data!)
+                self.cacheModel(model)
             }
             runCallbackOnMainThread(model)
         }
     }
-    
+
     public func validateSeed(seed:NSString, callback:(Bool?) -> Void) {
         Alamofire.request(.POST, validateSeedUrl, parameters:["seed":seed], encoding: .JSON)
                 .responseJSON { (response) -> Void in
--- QR/QRTests/SeedManagerTests.swift
+++ QR/QRTests/SeedManagerTests.swift
@@ -10,132 +10,129 @@ import XCTest
 @testable import QR
 
 class MockedSeedManager : SeedManager {
-    var cachedInputData: NSData?
-    var httpInputData: NSData?
-    var cachedData: NSData?
-    
-    init(cachedData: NSData?, httpData: NSData?) {
-        cachedInputData = cachedData
-        httpInputData = httpData
+    var cachedInputModel: SeedModel?
+    var httpInputModel: SeedModel?
+    var cachedOutputModel: SeedModel?
+
+    init(cachedData: SeedModel?, httpData: SeedModel?) {
+        cachedInputModel = cachedData
+        httpInputModel = httpData
         super.init()
     }
-    override internal func getDataFromCache() -> NSData? {
-        return cachedInputData
+    override internal func getModelFromCache() -> SeedModel? {
+        return cachedInputModel
     }
-    override internal func getDataFromServer(callback: (NSData?) -> Void) {
-        callback(httpInputData)
+    override internal func getModelFromServer(callback: (SeedModel?) -> Void) {
+        callback(httpInputModel)
     }
-    override internal func cacheData(data: NSData) {
-        print("cache data \(data)")
-        cachedData = data
+    override internal func cacheModel(model: SeedModel) {
+        cachedOutputModel = model
     }
 }
 
 class SeedManagerTests: XCTestCase {
-    func getData(seed:NSString, expiredDate:NSDate) -> NSData {
+    func getData(seed:NSString, expiredDate:NSDate) -> SeedModel {
         let expiredAt = expiredDate.timeIntervalSince1970 * 1000
-        return "{\"seed\":\"\(seed)\",\"expiredAt\":\(expiredAt)}".dataUsingEncoding(NSUTF8StringEncoding)!
+        let data = "{\"seed\":\"\(seed)\",\"expiredAt\":\(expiredAt)}".dataUsingEncoding(NSUTF8StringEncoding)!
+        return SeedModel(data: data)!
     }
-    
+
     func testGetSeedAsyncNoCacheHttpOk() {
         let expectation = expectationWithDescription("getSeedAsync return ok when no cache but http ok")
 
         let data = getData("37790a1b728096b4141864f49159ad47", expiredDate: NSDate(timeIntervalSinceNow: 60))
-        let expectedModel = SeedModel(data: data)
-        
+
         let seedManager = MockedSeedManager(cachedData: nil, httpData: data)
         seedManager.getSeedAsync { seed in
             XCTAssertNotNil(seed)
-            XCTAssertEqual(expectedModel, seed)
-            XCTAssertEqual(seedManager.cachedData, data)
+            XCTAssertEqual(data, seed)
+            XCTAssertEqual(seedManager.cachedOutputModel, data)
             expectation.fulfill()
         }
-        
+
         waitForExpectationsWithTimeout(3) { error in
             if let error = error {
                 XCTFail("waitForExpectationsWithTimeout errored: \(error)")
             }
         }
     }
-    
+
     func testGetSeedAsyncNoCacheHttpFail() {
         let expectation = expectationWithDescription("getSeedAsync return nil when no cache, http fail")
         let seedManager = MockedSeedManager(cachedData: nil, httpData: nil)
         seedManager.getSeedAsync { seed in
             XCTAssertNil(seed)
-            XCTAssertNil(seedManager.cachedData)
+            XCTAssertNil(seedManager.cachedOutputModel)
             expectation.fulfill()
         }
-        
+
         waitForExpectationsWithTimeout(3) { error in
             if let error = error {
                 XCTFail("waitForExpectationsWithTimeout errored: \(error)")
             }
         }
     }
-    
+
     func testGetSeedAsyncCacheValid() {
         let expectation = expectationWithDescription("getSeedAsync return ok when cache is valid")
         let theSeed = "37790a1b728096b4141864f49159ad47"
         let data = getData(theSeed, expiredDate: NSDate(timeIntervalSinceNow: 60))
-        let expectedModel = SeedModel(data: data)
-        
+
         let seedManager = MockedSeedManager(cachedData: data, httpData: nil)
         seedManager.getSeedAsync { seed in
             XCTAssertNotNil(seed)
-            XCTAssertEqual(expectedModel, seed)
-            XCTAssertNil(seedManager.cachedData)
+            XCTAssertEqual(data, seed)
+            XCTAssertNil(seedManager.cachedOutputModel)
             expectation.fulfill()
         }
-        
+
         waitForExpectationsWithTimeout(3) { error in
             if let error = error {
                 XCTFail("waitForExpectationsWithTimeout errored: \(error)")
             }
         }
     }
-    
+
     func testGetSeedAsyncCacheInvalidHttpOk() {
         let expectation = expectationWithDescription("getSeedAsync return ok when cache invalid http ok")
         let theSeed = "37790a1b728096b4141864f49159ad47"
         let cachedData = getData(theSeed, expiredDate: NSDate(timeIntervalSinceNow: -10))
         let httpData = getData(theSeed, expiredDate: NSDate(timeIntervalSinceNow: 60))
-        let expectedModel = SeedModel(data: httpData)
-        
+
         let seedManager = MockedSeedManager(cachedData: cachedData, httpData: httpData)
         seedManager.getSeedAsync { seed in
             XCTAssertNotNil(seed)
-            XCTAssertEqual(expectedModel, seed)
-            XCTAssertEqual(seedManager.cachedData, httpData)
+            XCTAssertEqual(httpData, seed)
+            XCTAssertEqual(seedManager.cachedOutputModel, httpData)
             expectation.fulfill()
         }
-        
+
         waitForExpectationsWithTimeout(3) { error in
             if let error = error {
                 XCTFail("waitForExpectationsWithTimeout errored: \(error)")
             }
         }
     }
-    
+
     func testGetSeedAsyncCacheInvalidHttpFailed() {
         let expectation = expectationWithDescription("getSeedAsync return nil when cache invalid, http failed")
         let theSeed = "37790a1b728096b4141864f49159ad47"
         let cachedData = getData(theSeed, expiredDate: NSDate(timeIntervalSinceNow: -10))
-        
+
         let seedManager = MockedSeedManager(cachedData: cachedData, httpData: nil)
         seedManager.getSeedAsync { seed in
             XCTAssertNil(seed)
-            XCTAssertNil(seedManager.cachedData)
+            XCTAssertNil(seedManager.cachedOutputModel)
             expectation.fulfill()
         }
-        
+
         waitForExpectationsWithTimeout(3) { error in
             if let error = error {
                 XCTFail("waitForExpectationsWithTimeout errored: \(error)")
             }
         }
     }
-    
+
     func testGetSeedAsync() {
         let expectation = expectationWithDescription("getSeedAsync work with server")
         let seedManager = SeedManager()
