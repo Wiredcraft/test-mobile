5c836ceafbf8c65f9a2ad70311e95c9f1d89ef3d
ShuRong Deng <dengshurong@ezbuy.com>
2017-07-07 21:36:42 +0800

    use GravitySquareView in ViewController

--- Simple-Animation-iOS/SimpleAnimationExample/SimpleAnimationExample/ViewController.swift
+++ Simple-Animation-iOS/SimpleAnimationExample/SimpleAnimationExample/ViewController.swift
@@ -6,20 +6,28 @@
 //  Copyright © 2017 ShuRong Deng. All rights reserved.
 //
 
+
+
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var gravitySquareView : GravtitySquareView?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let screenSize = UIScreen.main.bounds
+        gravitySquareView = GravtitySquareView(frame: CGRect(x: screenSize.width/2, y: screenSize.height/2, width: 80, height: 80))
+        gravitySquareView?.backgroundColor = UIColor.darkGray
+        self.view.addSubview(gravitySquareView!)
+        gravitySquareView?.start()
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
 
 }
 
