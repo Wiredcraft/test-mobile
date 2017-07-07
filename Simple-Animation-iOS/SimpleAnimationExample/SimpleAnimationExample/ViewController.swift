1cdf46c931edcaa15467e670fa9d858f5a6f00cc
ShuRong Deng <dengshurong@ezbuy.com>
2017-07-07 13:13:53 +0800

    init project

---
+++ Simple-Animation-iOS/SimpleAnimationExample/SimpleAnimationExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleAnimationExample
+//
+//  Created by ShuRong Deng on 07/07/2017.
+//  Copyright © 2017 ShuRong Deng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
