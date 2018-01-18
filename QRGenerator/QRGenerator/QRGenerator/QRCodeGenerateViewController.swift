c73a4f07788c35565e285aca392e90b8dd591bd7
杨志超 <yzc1@meitu.com>
2018-01-18 14:06:34 +0800

    【Feature】Create iOS project

---
+++ QRGenerator/QRGenerator/QRGenerator/QRCodeGenerateViewController.swift
@@ -0,0 +1,13 @@
+//
+//  QRCodeGenerateViewController.swift
+//  QRGenerator
+//
+//  Created by buginux on 2018/1/18.
+//  Copyright © 2018年 buginux. All rights reserved.
+//
+
+import UIKit
+
+class QRCodeGenerateViewController: UIViewController {
+    
+}
---
+++ QRGenerator/QRGenerator/QRGenerator/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  QRGenerator
+//
+//  Created by buginux on 2018/1/18.
+//  Copyright © 2018年 buginux. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
