99bef88759c90b1af70f450c35220038a372a692
tripleCC <triplec.linux@gmail.com>
2018-01-23 19:16:09 +0800

    feat(App): add seed model

--- QR-generator/QRGenerator/QRGenerator/Seed.swift
+++ QR-generator/QRGenerator/QRGenerator/Seed.swift
@@ -7,3 +7,20 @@
 //
 
 import Foundation
+import ObjectMapper
+
+struct Seed: Mappable {
+    var seed: String?
+    var expiresAt: String?
+    
+    init() {
+    }
+    
+    init?(map: Map) {
+    }
+    
+    mutating func mapping(map: Map) {
+        seed        <- map["seed"]
+        expiresAt   <- map["expiresAt"]
+    }
+}
