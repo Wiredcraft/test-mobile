2fb85e9771e88b1d4a1db1d1bde92d3d7d43202b
tripleCC <triplec.linux@gmail.com>
2018-01-23 19:16:39 +0800

    feat(App): add QRCodeGenerator and QRCodeReader

--- QR-generator/QRGenerator/QRGenerator/QRCodeGenerator.swift
+++ QR-generator/QRGenerator/QRGenerator/QRCodeGenerator.swift
@@ -6,4 +6,24 @@
 //  Copyright © 2018年 tripleCC. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+public class QRCodeGenerator {
+    private var data: Data
+    
+    init(string: String) {
+        self.data = string.data(using: .isoLatin1, allowLossyConversion: false)!
+    }
+    
+    init(data: Data) {
+        self.data = data
+    }
+    
+    public func generate() -> UIImage? {
+        let filter = CIFilter(name: "QRCodeGenerator")
+        filter?.setValue(data, forKey: "inputMessage")
+        filter?.setValue("Q", forKey: "inputCorrectionLevel")
+        
+        return filter?.outputImage.flatMap { UIImage(ciImage: $0) }
+    }
+}
