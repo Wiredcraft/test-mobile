f8ab641274b160eb921bcd8e00d7f4b8cb9c466d
dataegg <dxdroid@gmail.com>
2018-03-11 18:54:24 +0800

    add get seed unit test

--- QRGenerator/app/src/androidTest/java/com/seazon/qrgenerator/ExampleInstrumentedTest.kt
+++ QRGenerator/app/src/androidTest/java/com/seazon/qrgenerator/ExampleInstrumentedTest.kt
@@ -2,11 +2,17 @@ package com.seazon.qrgenerator
 
 import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
+import com.seazon.qrgenerator.api.ApiManager
+import com.seazon.qrgenerator.entity.Seed
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.io.IOException
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -20,5 +26,23 @@ class ExampleInstrumentedTest {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
         assertEquals("com.seazon.qrgenerator", appContext.packageName)
+    }
+
+    @Test
+    fun getSeedApi() {
+        ApiManager.getInstence().getApiService().getSeed().enqueue(object : Callback<Seed> {
+            override fun onResponse(call: Call<Seed>, response: Response<Seed>) {
+                try {
+                    assertNotNull(response.body()::class)
+                    assertEquals(response.body()::class, Seed::class)
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+            }
+
+            override fun onFailure(call: Call<Seed>, t: Throwable) {
+                t.printStackTrace()
+            }
+        })
     }
 }
