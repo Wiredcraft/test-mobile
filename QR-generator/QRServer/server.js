0715a887c9e6b0a4edf30122ff9043faad7380cb
MelonRice <lihongwei850718@gmail.com>
2018-07-25 23:48:15 +0800

    Add comment for server.js

--- QR-generator/QRServer/server.js
+++ QR-generator/QRServer/server.js
@@ -1,17 +1,20 @@
 var express = require('express');
 var app = express();
+// generate random string
 var random = require("randomstring");
+// a lite,immutable date library
 var dayjs = require('dayjs');
 
 app.route('/seed')
   .get(function (req, res) {
     // expires at 1 minute later
     var expires_at = dayjs().add(1, 'minute');
+    // build response body
     var seed = { "seed": random.generate(), "expires_at": expires_at };
     res.json(seed);
   });
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 3600;
 app.listen(port);
 										
 
