e48207ce4ad9cfec1e56315a702b031c1e46af47
MelonRice <lihongwei850718@gmail.com>
2018-07-26 09:59:55 +0800

    Add stetho

--- QR-generator/QRClient/app/src/main/java/com/craft/qrgenerator/MyApplication.kt
+++ QR-generator/QRClient/app/src/main/java/com/craft/qrgenerator/MyApplication.kt
@@ -1,12 +1,17 @@
 package com.craft.qrgenerator
 
 import android.app.Application
+import com.facebook.stetho.Stetho
 import com.uuzuche.lib_zxing.activity.ZXingLibrary
 
 class MyApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        // Init ZXing for QR generate and scan
         ZXingLibrary.initDisplayOpinion(this)
+        // Init Stetho for network listener
+        // input  chrome://inspect  in browser address field
+        Stetho.initializeWithDefaults(this)
     }
 }
