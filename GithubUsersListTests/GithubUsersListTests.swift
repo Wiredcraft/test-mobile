5cafd6eff10fcc06b3a368746715ac0734b2445b
Nick <nick.qiu@cootek.cn>
2021-03-10 12:03:14 +0800

    add unit test

--- GithubUsersListTests/GithubUsersListTests.swift
+++ GithubUsersListTests/GithubUsersListTests.swift
@@ -6,9 +6,13 @@
 //
 
 import XCTest
+import RxSwift
+import RxRelay
 @testable import GithubUsersList
 
 class GithubUsersListTests: XCTestCase {
+    
+    let disposeBag = DisposeBag()
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -29,5 +33,108 @@ class GithubUsersListTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
-
+    
+    func testUsersListForSearch() {
+        let viewModel = GULUsersListViewModel()
+        let search = BehaviorRelay<String>(value: "")
+        let header = PublishRelay<Void>()
+        let footer = PublishRelay<Void>()
+        let input = GULUsersListViewModel.GULUsersListInput(search: search, headerRefresh: header, footerRefresh: footer)
+        
+        let output = viewModel.transform(input: input)
+        
+        
+        let expext = expectation(description: "search")
+        
+        output.usersItems.skip(1).subscribe { (list) in
+            expext.fulfill()
+        }.disposed(by: disposeBag)
+        
+        XCTAssert(output.usersItems.value.count == 0, "shouldn't have users data")
+        input.search.accept("swift")
+        waitForExpectations(timeout: 30) { (error) in
+            guard error == nil else {
+                XCTFail(error!.localizedDescription)
+                return
+            }
+            XCTAssert(viewModel.currentPage == 1, "the page of search action is wrong")
+            XCTAssert(output.usersItems.value.count > 0, "no users data")
+            output.usersItems.value.forEach { (vm) in
+                XCTAssert(vm.name.value?.isEmpty == false, "no user's name")
+                XCTAssert(vm.htmlUrl.value?.isEmpty == false, "no user's htmlUrl")
+                XCTAssert(vm.icon.value?.isEmpty == false, "no user's icon")
+                XCTAssertNotNil(vm.score.value, "no user's score")
+            }
+        }
+    }
+    
+    
+    func testUsersListForHeader() {
+        let viewModel = GULUsersListViewModel()
+        let search = BehaviorRelay<String>(value: "sss")
+        let header = PublishRelay<Void>()
+        let footer = PublishRelay<Void>()
+        let input = GULUsersListViewModel.GULUsersListInput(search: search, headerRefresh: header, footerRefresh: footer)
+        
+        let output = viewModel.transform(input: input)
+        
+        
+        let expext = expectation(description: "header")
+        
+        output.usersItems.skip(1).subscribe { (list) in
+            expext.fulfill()
+        }.disposed(by: disposeBag)
+        
+        XCTAssert(output.usersItems.value.count == 0, "shouldn't have users data")
+        input.headerRefresh.accept(())
+        waitForExpectations(timeout: 30) { (error) in
+            guard error == nil else {
+                XCTFail(error!.localizedDescription)
+                return
+            }
+            XCTAssert(viewModel.currentPage == 1, "the page of header action is wrong")
+            XCTAssert(output.usersItems.value.count > 0, "no users data")
+            output.usersItems.value.forEach { (vm) in
+                XCTAssert(vm.name.value?.isEmpty == false, "no user's name")
+                XCTAssert(vm.htmlUrl.value?.isEmpty == false, "no user's htmlUrl")
+                XCTAssert(vm.icon.value?.isEmpty == false, "no user's icon")
+                XCTAssertNotNil(vm.score.value, "no user's score")
+            }
+        }
+    }
+    
+    
+    func testUsersListForFooter() {
+        let viewModel = GULUsersListViewModel()
+        let search = BehaviorRelay<String>(value: "sss")
+        let header = PublishRelay<Void>()
+        let footer = PublishRelay<Void>()
+        let input = GULUsersListViewModel.GULUsersListInput(search: search, headerRefresh: header, footerRefresh: footer)
+        
+        let output = viewModel.transform(input: input)
+        
+        
+        let expext = expectation(description: "footer")
+        
+        output.usersItems.skip(1).subscribe { (list) in
+            expext.fulfill()
+        }.disposed(by: disposeBag)
+        
+        XCTAssert(output.usersItems.value.count == 0, "shouldn't have users data")
+        input.footerRefresh.accept(())
+        waitForExpectations(timeout: 30) { (error) in
+            guard error == nil else {
+                XCTFail(error!.localizedDescription)
+                return
+            }
+            XCTAssert(viewModel.currentPage > 1, "the page of footer action is wrong")
+            XCTAssert(output.usersItems.value.count > 0, "no users data")
+            output.usersItems.value.forEach { (vm) in
+                XCTAssert(vm.name.value?.isEmpty == false, "no user's name")
+                XCTAssert(vm.htmlUrl.value?.isEmpty == false, "no user's htmlUrl")
+                XCTAssert(vm.icon.value?.isEmpty == false, "no user's icon")
+                XCTAssertNotNil(vm.score.value, "no user's score")
+            }
+        }
+    }
 }
