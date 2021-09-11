202046973cc78fe032210c9e5f3792c5942a9845
caizhixing <herelookingatyoukid@gmail.com>
2021-09-11 15:13:15 +0800

    add a empty UserListActivity to make this project can run as a App

---
+++ GithubApiDemo/app/src/main/java/com/caizhixng/githubapidemo/UserListActivity.kt
@@ -0,0 +1,11 @@
+package com.caizhixng.githubapidemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UserListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_list)
+    }
+}
