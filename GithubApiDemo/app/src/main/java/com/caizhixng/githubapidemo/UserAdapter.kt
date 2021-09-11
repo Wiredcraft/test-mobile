acae3cebf2d87636fb8215e605b6ae5b7c4bd7d6
caizhixing <herelookingatyoukid@gmail.com>
2021-09-11 18:20:38 +0800

    show user avatar
    1.add Glide to show avatar
    2.change  UI layout to feed demand

--- GithubApiDemo/app/src/main/java/com/caizhixng/githubapidemo/UserAdapter.kt
+++ GithubApiDemo/app/src/main/java/com/caizhixng/githubapidemo/UserAdapter.kt
@@ -1,5 +1,7 @@
 package com.caizhixng.githubapidemo
 
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CircleCrop
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.module.LoadMoreModule
 import com.chad.library.adapter.base.viewholder.BaseViewHolder
@@ -13,6 +15,12 @@ class UserAdapter : BaseQuickAdapter<User, BaseViewHolder>(R.layout.item_user), 
         holder.setText(R.id.user_name, user.userName)
         holder.setText(R.id.user_score, user.userScore)
         holder.setText(R.id.user_url, user.userUrl)
+
+        Glide.with(holder.itemView).load(user.userUrl)
+            .placeholder(R.drawable.ic_baseline_account_circle)
+            .error(R.drawable.ic_baseline_account_circle)
+            .transform(CircleCrop())
+            .into(holder.getView(R.id.user_avatar))
     }
 
 }
