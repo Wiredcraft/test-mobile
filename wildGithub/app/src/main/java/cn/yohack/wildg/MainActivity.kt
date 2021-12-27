4bbe6d76b28890dddf4515e4d5c59bd5df9005d4
yohack <yo.hackm@gmail.com>
2021-12-27 18:20:37 +0800

    init project

---
+++ wildGithub/app/src/main/java/cn/yohack/wildg/MainActivity.kt
@@ -0,0 +1,11 @@
+package cn.yohack.wildg
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
