910e1a36dac1287934098459a1c08cfea83cd2a5
Jun Ma <wajmjjj@gmail.com>
2022-03-21 12:09:02 +0800

    Init project and add .gitignore file

---
+++ test_mobile/test_mobile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test_mobile
+//
+//  Created by Jun Ma on 2022/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ test_mobile/test_mobileUITests/test_mobileUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  test_mobileUITestsLaunchTests.swift
+//  test_mobileUITests
+//
+//  Created by Jun Ma on 2022/3/21.
+//
+
+import XCTest
+
+class test_mobileUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
