c384ff34ac2035b7b5942c7f35c86c50bb9debe8
devlinlin <chandler7k@hotmail.com>
2022-05-12 00:34:21 +0800

    [update]rarrange group

--- GitHub-Users-List-App/GitHub-Users-List-App/Domain/UseCase/UsersListUseCase.swift
+++ GitHub-Users-List-App/GitHub-Users-List-App/Domain/UseCase/UsersListUseCase.swift
@@ -13,9 +13,11 @@ protocol UsersListUseCase {
 
 final class DefaultUserListUseCase: UsersListUseCase {
     private let usersListRepository: UsersRepository
+
     init(usersListRepository: UsersRepository) {
         self.usersListRepository = usersListRepository
     }
+
     func excute(requestValue: UsersQueryUseCaseRequestValue, completion: @escaping (Result<UsersListPage, Error>) -> Void) -> Cancellable? {
         return usersListRepository.fectchUsersList(query: requestValue) { result in
             completion(result)
@@ -27,6 +29,7 @@ struct UsersQueryUseCaseRequestValue {
     let q: String
     let page: Int
     var per_page: Int = 30
+    
     func query() -> UsersQuery {
         return UsersQuery(q: q, page: page, per_page: per_page)
     }
