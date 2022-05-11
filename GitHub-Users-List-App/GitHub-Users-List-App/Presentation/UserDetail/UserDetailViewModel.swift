7c14f8deff21b247eb3287ed561e91091b1a31ac
devlinlin <chandler7k@hotmail.com>
2022-05-11 21:22:12 +0800

    [update] add UserDetailViewModel.swift

--- GitHub-Users-List-App/GitHub-Users-List-App/Presentation/UserDetail/UserDetailViewModel.swift
+++ GitHub-Users-List-App/GitHub-Users-List-App/Presentation/UserDetail/UserDetailViewModel.swift
@@ -7,3 +7,36 @@
 
 import Foundation
 
+protocol UserDetailViewModelInputs {
+    func viewDidLoad()
+}
+
+protocol UserDetailViewModelOutputs {
+
+}
+
+struct UserDetailViewModelActions {
+    let followUser: (User) -> Void
+}
+
+protocol UserDetailViewModelType {
+    var inputs: UserDetailViewModelInputs { get }
+    var outputs: UserDetailViewModelOutputs { get }
+}
+
+final class UserDetailViewModel: UserDetailViewModelType, UserDetailViewModelInputs, UserDetailViewModelOutputs {
+    var inputs: UserDetailViewModelInputs { return self }
+    var outputs: UserDetailViewModelOutputs { return self }
+    private let actions: UserDetailViewModelActions
+    private let usecase: UserDetailUseCase
+    init(with actions: UserDetailViewModelActions, usecase: UserDetailUseCase) {
+        self.actions = actions
+        self.usecase = usecase
+    }
+}
+
+extension UserDetailViewModel {
+    func viewDidLoad() {
+
+    }
+}
